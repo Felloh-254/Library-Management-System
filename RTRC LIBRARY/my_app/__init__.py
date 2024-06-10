42164deb86fc6369534f1e31706e0fa4b09d5ce6
Felloh-254 <108602238+Felloh-254@users.noreply.github.com>
2024-06-10 09:52:08 +0300

    Update __init__.py

--- RTRC LIBRARY/my_app/__init__.py
+++ RTRC LIBRARY/my_app/__init__.py
@@ -30,11 +30,11 @@ def create_app():
 	app.config["SECRET_KEY"] = secret_key
 
 	# Configure Flask-Mail for Gmail (using app password if available)
-	app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+	app.config['MAIL_SERVER'] = 'your_mail_server.com/IP Address'
 	app.config['MAIL_PORT'] = 587
 	app.config['MAIL_USE_TLS'] = True
-	app.config['MAIL_USERNAME'] = 'felixogweny@gmail.com'
-	app.config['MAIL_PASSWORD'] = 'cuav unlr mvim slwh'
+	app.config['MAIL_USERNAME'] = 'your_email@your_mail_serve.com'
+	app.config['MAIL_PASSWORD'] = 'mail_server_password'
 
 	#initiating the db with the app
 	db.init_app(app)
